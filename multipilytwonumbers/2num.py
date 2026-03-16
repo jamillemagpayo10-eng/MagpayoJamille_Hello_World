d2ba64150713a10a52a752d917621e297c8f519b
jamillemagpayo10-eng <jamillemagpayo10@gmail.com>
2026-03-16 20:41:03 +0800

    Add files via upload

---
+++ multipilytwonumbers/2num.py
@@ -0,0 +1,8 @@
+# Multiply Two Numbers
+
+MagpayoNum1 = int(input("Enter first number: "))
+MagpayoNum2 = int(input("Enter second number: "))
+
+MagpayoResult = MagpayoNum1 * MagpayoNum2
+
+print("Result:", MagpayoResult)
